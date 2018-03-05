87cf758af0cef15bc74b031ac853cb120d11a81c
Jason O'Dwyer <JasonODwyer@jasons-air.andoverma.byod>
2018-03-05 12:35:12 -0500

    Added JavaArcae file

---
+++ JavaArcade.java
@@ -0,0 +1,22 @@
+
+public interface JavaArcade
+{
+    public boolean running();
+    
+    public void startGame();
+    
+    public String getGameName();
+    
+    public void pauseGame();
+    
+    public String getInstructions();
+    
+    public String getCredits();
+    
+    public String getHighScore();
+    
+    public void stopGame();
+    
+    public int getPoints();
+    
+}
