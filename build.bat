e22b8c86a3326db17f11620db81de479f071ab34
Jason A. Yundt <swagfortress@gmail.com>
2018-03-10 08:18:34 -0500

    Windows build script now copies assets as well

--- build.bat
+++ build.bat
@@ -5,3 +5,12 @@ IF NOT EXIST build (
 )
 
 javac -d build src/*.java
+
+REM Copy the assets
+IF EXIST build/assets (
+    rmdir /S /Q build\assets
+)
+
+mkdir build\assets
+copy assets build\assets\ >NUL
+REM send stdout to NUL for a silent copy
