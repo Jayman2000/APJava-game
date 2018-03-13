95837f8986d0d925c106faea6382f812bc53f9e3
Jason A. Yundt <swagfortress@gmail.com>
2018-03-13 13:04:44 -0400

    Player is now a Renderable

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -2,16 +2,19 @@ import java.awt.Image;
 
 import java.io.IOException;
 
+import java.util.ArrayList;
+
 public class GameLogic
 {
     public final static int WIDTH  = 640;
     public final static int HEIGHT = 360;
 
-    private Ball ball;
+    private ArrayList<Renderable> toDraw; // Will be drawn on every call to update
 
     public GameLogic()
     {
-        ball = new Ball(WIDTH/2.0, HEIGHT/2.0, 1, 1);
+        toDraw = new ArrayList<Renderable>();
+        toDraw.add(new Player());
     }
 
     /* Runs one 'tic' of game logic.
@@ -23,19 +26,25 @@ public class GameLogic
      * Returns: What the client should draw, in the order the client should
      *          draw it.
      *
-     * Precondition(s) : None
+     * Precondition(s) : toDraw and all of its elements are not null.
      * Postcondition(s): All entities which respond to the player's input have
      *                   been informed of the input, and all game state has
      *                   been updated for deltaTime milliseconds of game play.
      *
-     * Author(s): None (stub)
+     * Author(s): Jason Yundt
      */
     public OutputInfo update(Object[] inputSignals, int deltaTime)
     {
-        ball.update(deltaTime);
+        ArrayList<RenderInfo> retVisuals = new ArrayList<RenderInfo>(toDraw.size());
 
-        RenderInfo[] toDraw = new SwingRenderInfo[1];
-        toDraw[0] = ball.getRenderInfo();
-        return new OutputInfo(toDraw);
+        for(Renderable r : toDraw)
+        {
+            for(RenderInfo toAdd : r.getRenderInfo())
+            {
+                retVisuals.add(toAdd);
+            }
+        }
+
+        return new OutputInfo(retVisuals.toArray(new RenderInfo[retVisuals.size()]));
     }
 }
