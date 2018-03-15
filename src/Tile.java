a81717de86d312e9a584b5878aa945cd12c469f8
Jason O'Dwyer <JasonODwyer@jasons-air.fios-router.home>
2018-03-14 21:55:04 -0400

    Added Tile's collision with Ball

--- src/Tile.java
+++ src/Tile.java
@@ -1,18 +1,26 @@
 
-public class Tile extends Circle
+public class Tile extends Circle implements Collidable
 {
-	private final Object sprite;
+    private final Object sprite;
 	
-	public Tile(double x, double y, double width)
-	{
-		super(x, y, (width/2.0));
-		sprite = Game.loadSprite("tile.png");
-	}
+    public Tile(double x, double y, double width)
+    {
+        super(x, y, (width/2.0));
+        sprite = Game.loadSprite("tile.png");
+    }
 	
-	public RenderInfo[] getRenderInfo()
-	{
-		RenderInfo[] ret = new RenderInfo[1];
-		ret[0] = Game.newRenderInfo(sprite, getX()-getRadius(), getY()-getRadius());
-		return ret;
-	}
+    public RenderInfo[] getRenderInfo()
+    {
+        RenderInfo[] ret = new RenderInfo[1];
+        ret[0] = Game.newRenderInfo(sprite, getX()-getRadius(), getY()-getRadius());
+        return ret;
+    }
+
+    public void collision(Ball b)
+    {
+        if(this.isColliding(b))
+        {
+            sprite = Game.loadSprite(tile2.png);
+            /* Add point */
+        }
 }
