152d5fe8fc86a23468d4f9de24bb33416b4c2738
Jason A. Yundt <swagfortress@gmail.com>
2018-04-15 14:06:35 -0400

    Added script to generate jar file

---
+++ create-jar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./build.sh
+if [ -d "dist" ]; then
+    rm -r dist
+fi
+mkdir dist
+
+cd build
+jar cfm ../dist/Yours.jar ../src/manifest.txt *.class
+cd ..
+chmod +x dist/Yours.jar
+# Copy the assets
+cp -r assets dist/assets
